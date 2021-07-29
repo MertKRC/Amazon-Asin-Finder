740543c5dc84bcd55b00f8fe562a8f2ea6702e5b
Mert KARACA <selefimert41@gmail.com>
2021-07-29 17:15:39 +0300

    Updated code with comments for better readability.

--- AsinAppAmazon/Form1.cs
+++ AsinAppAmazon/Form1.cs
@@ -16,25 +16,27 @@ namespace AsinAppAmazon
         public Form1()
         {
             InitializeComponent();
-            //webBrowser1.Navigate("http://www.google.com");
-            //textBox1.Text = "http://www.google.com";
-            webBrowser1.Navigate("https://www.amazon.ca/s?i=merchant-items&me=A1U6PAVBPLQVBI");
+            //You may enter any amazon merchant's address here, also you can search it from typing address to URL section and pressing search button.
+            //Just type https://www.google.com if you want to make it homepage.
             textBox1.Text = "https://www.amazon.ca/s?i=merchant-items&me=A1U6PAVBPLQVBI";
+            webBrowser1.Navigate(textBox1.Text);
             label5.Text = "1";
         }
 
+        //Search Button
         private void button1_Click(object sender, EventArgs e)
         {
             string url = textBox1.Text;
             webBrowser1.Navigate(url);
         }
 
+        //Updating HTML Code section when browser completed,
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             richTextBox1.Text = webBrowser1.DocumentText;
-
         }
 
+        //Main algorithm for get ASIN data from each page
         private void GetAsin()
         {
             if (webBrowser1.Document != null)
@@ -54,8 +56,9 @@ namespace AsinAppAmazon
             }
         }
 
+        //Needed for count the page and print it to label
         int count = 2;
-
+        //For turn the page
         private void TurnPage()
         {
             
@@ -82,18 +85,21 @@ namespace AsinAppAmazon
             }
         }
 
+        //What happens when timer tick;
         private void timer1_Tick(object sender, EventArgs e)
         {
             GetAsin();
             TurnPage();
         }
 
+        //Start button
         private void button5_Click(object sender, EventArgs e)
         {
             timer1.Enabled = true;
             timer1.Interval = 5000;
         }
 
+        //Stop button
         private void button6_Click(object sender, EventArgs e)
         {
             timer1.Enabled = false;
